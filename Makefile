ca257797e6f9a0aedf9a50374ca576fa2e857ccd
Nico Braun <rainbowstack@gmail.com>
2022-04-11 20:09:23 +0200

    move decoder flag up in help output
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-ver=0.1.0
+ver=0.1.1
 bindir=bin
 container_cli=docker
 cmd_dir=./cmd/tpl/
--- cmd/tpl/tpl.go
+++ cmd/tpl/tpl.go
@@ -26,7 +26,7 @@ var (
 	options      []string
 	decoder      string
 	noNewline    bool
-	version      = "0.1.0"
+	version      = "0.1.1"
 	commit       = "unknown"
 	debugMode    bool
 	reflectMode  bool
@@ -219,8 +219,8 @@ func parseFlags() {
 	flag.StringArrayVarP(&files, "file", "f", []string{}, "template file path. Can be specified multiple times")
 	flag.StringArrayVarP(&globs, "glob", "g", []string{}, "template file glob. Can be specified multiple times")
 	flag.StringVarP(&templateName, "name", "n", "", "if specified, execute the template with the given name")
-	flag.StringArrayVar(&options, "options", []string{}, "options to pass to the template engine")
 	flag.StringVarP(&decoder, "decoder", "d", "json", "decoder to use for input data. Supported values: json, yaml, toml, xml")
+	flag.StringArrayVar(&options, "options", []string{}, "options to pass to the template engine")
 	flag.BoolVar(&noNewline, "no-newline", false, "do not print newline at the end of the output")
 	flag.BoolVarP(&showHelp, "help", "h", false, "show the help text")
 	flag.BoolVarP(&showVersion, "version", "v", false, "show the version")
@@ -230,7 +230,7 @@ func parseFlags() {
 		os.Exit(0)
 	}
 	if showVersion {
-		fmt.Printf("version: %s, commit: %s\n", version, commit)
+		fmt.Printf("version %s - commit %s\n", version, commit)
 		os.Exit(0)
 	}
 
