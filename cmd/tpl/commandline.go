bc7ded7b4d0af922b24bdaa31e99b3a70bde2fdf
Nico Braun <rainbowstack@gmail.com>
2023-08-11 13:06:55 +0200

    refactor: commandline
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- cmd/tpl/commandline.go
+++ cmd/tpl/commandline.go
@@ -13,6 +13,7 @@ import (
 	"github.com/spf13/pflag"
 )
 
+// the options are used to control the runtime behavior of the commandLine
 type commandLineOptions struct {
 	// internal
 	defaultTemplateName string
@@ -25,6 +26,7 @@ type commandLineOptions struct {
 	noNewline    bool
 }
 
+// generate options with default values
 func defaultOptions() *commandLineOptions {
 	return &commandLineOptions{
 		defaultTemplateName: "_gotpl_default",
@@ -32,6 +34,8 @@ func defaultOptions() *commandLineOptions {
 	}
 }
 
+// link the opt struct fields to the given flagset. defaults to
+// pflag.CommandLine. requires to call fs.Parse, AFTER calling initFlags
 func (opts *commandLineOptions) initFlags(fs *pflag.FlagSet) {
 	if fs == nil {
 		fs = pflag.CommandLine
@@ -44,6 +48,7 @@ func (opts *commandLineOptions) initFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&opts.noNewline, "no-newline", opts.noNewline, "do not print newline at the end of the output")
 }
 
+// run the template cli program
 func commandLine(ctx context.Context, args []string, input io.Reader, output io.Writer) (err error) {
 	// parse the pflag set
 	fs := pflag.NewFlagSet("tpl", pflag.ContinueOnError)
@@ -57,46 +62,18 @@ func commandLine(ctx context.Context, args []string, input io.Reader, output io.
 	}
 
 	// create the default template
-	tpl := template.New(opts.defaultTemplateName)
-	tpl.Option(opts.options...)
-	tpl.Funcs(textfunc.MapClosure(sprig.TxtFuncMap(), tpl))
+	tpl := baseTemplate(opts.defaultTemplateName, opts.options...)
 
-	// parse the arguments
-	for _, arg := range fs.Args() {
-		tpl, err = tpl.Parse(arg)
-		if err != nil {
-			return fmt.Errorf("error to parsing template: %v", err)
-		}
+	// parse positional arguments
+	tpl, err = parsePosArgs(tpl, fs.Args())
+	if err != nil {
+		return fmt.Errorf("parse pos args: %w", err)
 	}
 
-	// FIXME if arg is like --file=foo.txt,
-	// the if conditions wont detect it
-	//
-	// parse files and globs in the order they were specified
-	// to align with go's template package
-	fileIndex := 0
-	globIndex := 0
-	for _, arg := range args {
-		if arg == "-f" || arg == "--file" {
-			// parse next file
-			file := opts.files[fileIndex]
-			tpl, err = tpl.ParseFiles(file)
-			if err != nil {
-				return fmt.Errorf("error parsing file %s: %v", file, err)
-			}
-			fileIndex++
-			continue
-		}
-		if arg == "-g" || arg == "--glob" {
-			// parse next glob
-			glob := opts.globs[globIndex]
-			tpl, err = tpl.ParseGlob(glob)
-			if err != nil {
-				return fmt.Errorf("error parsing glob %s: %v", glob, err)
-			}
-			globIndex++
-			continue
-		}
+	// parse optional arguments
+	tpl, err = parseOptArgs(tpl, args, opts.files, opts.globs)
+	if err != nil {
+		return fmt.Errorf("parse opt args: %w", err)
 	}
 
 	// defined templates
@@ -108,17 +85,9 @@ func commandLine(ctx context.Context, args []string, input io.Reader, output io.
 	}
 
 	// determine the template to use
-	if opts.templateName == "" {
-		if len(fs.Args()) > 0 {
-			opts.templateName = opts.defaultTemplateName
-		} else if len(templates) == 1 {
-			opts.templateName = templates[0].Name()
-		} else {
-			return fmt.Errorf(
-				"the --name flag is required when multiple templates are defined and no default template exists%s",
-				tpl.DefinedTemplates(),
-			)
-		}
+	opts.templateName, err = selectedTemplate(opts.templateName, opts.defaultTemplateName, fs.Args(), templates)
+	if err != nil {
+		return fmt.Errorf("select template: %w", err)
 	}
 
 	// data is used to store the decoded input
@@ -131,8 +100,7 @@ func commandLine(ctx context.Context, args []string, input io.Reader, output io.
 		}
 	}
 
-	// execute the template with the given name
-	// and optional data from stdin
+	// execute the template with the given name and optional data from stdin
 	if err := tpl.ExecuteTemplate(output, opts.templateName, data); err != nil {
 		return fmt.Errorf("error executing template: %v", err)
 	}
@@ -143,4 +111,83 @@ func commandLine(ctx context.Context, args []string, input io.Reader, output io.
 	}
 
 	return nil
+}
+
+// construct a base templates with custom functions attached
+func baseTemplate(defaultName string, options ...string) *template.Template {
+	tpl := template.New(defaultName)
+	tpl = tpl.Option(options...)
+	tpl = tpl.Funcs(textfunc.MapClosure(sprig.TxtFuncMap(), tpl))
+	return tpl
+}
+
+// parse all positional arguments into the "default" template
+func parsePosArgs(tpl *template.Template, posArgs []string) (*template.Template, error) {
+	var err error
+	for _, arg := range posArgs {
+		tpl, err = tpl.Parse(arg)
+		if err != nil {
+			return nil, fmt.Errorf("error to parsing template: %v", err)
+		}
+	}
+	return tpl, nil
+}
+
+// parse files and globs in the order they were specified, to align with go's
+// template engine
+func parseOptArgs(tpl *template.Template, rawArgs []string, files, globs []string) (*template.Template, error) {
+	var (
+		err       error
+		fileIndex uint8
+		globIndex uint8
+	)
+
+	// FIXME if arg is like --file=foo.txt, the if conditions wont detect it
+
+	for _, arg := range rawArgs {
+		if arg == "-f" || arg == "--file" {
+			// parse next file
+			file := files[fileIndex]
+			tpl, err = tpl.ParseFiles(file)
+			if err != nil {
+				return nil, fmt.Errorf("error parsing file %s: %v", file, err)
+			}
+			fileIndex++
+			continue
+		}
+
+		if arg == "-g" || arg == "--glob" {
+			// parse next glob
+			glob := globs[globIndex]
+			tpl, err = tpl.ParseGlob(glob)
+			if err != nil {
+				return nil, fmt.Errorf("error parsing glob %s: %v", glob, err)
+			}
+			globIndex++
+			continue
+		}
+	}
+
+	return tpl, nil
+}
+
+// determine the template to execute. In the order of precedence:
+//  1. current name, if set
+//  2. default name, if at least 1 positional arg
+//  3. templates name, if exactly 1 template
+//  4. --name flag required, otherwise
+func selectedTemplate(currentName, defaultName string, posArgs []string, templates []*template.Template) (string, error) {
+	if currentName != "" {
+		return currentName, nil
+	}
+
+	if len(posArgs) > 0 {
+		return defaultName, nil
+	}
+
+	if len(templates) == 1 {
+		return templates[0].Name(), nil
+	}
+
+	return "", fmt.Errorf("the --name flag is required when multiple templates are defined and no default template exists")
 }
